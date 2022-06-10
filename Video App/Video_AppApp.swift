080e86d1d2224f86e17c71303fccea3e5e11ef86
Timal Pathirana <timalbathiya1995@gmail.com>
2022-06-10 12:48:52 +1000

    Initial Commit

---
+++ Video App/Video_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_AppApp.swift
+//  Video App
+//
+//  Created by Timal Pathirana on 10/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct Video_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
